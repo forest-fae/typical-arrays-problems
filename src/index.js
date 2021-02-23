9cbffca69267bebf8b4034dfaa62692bd8e44039
Coreen <foxy-bat@ya.ru>
2021-02-23 15:36:56 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array === undefined) {
+    return 0;
+  } else if(array.length === 0) {
+    return 0;
+  };
+let res = array.sort((a, b) => a - b);
+  return res[0];
+  };
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array === undefined) {
+    return 0;
+  } else if(array.length === 0) {
+    return 0;
+  };
+  let sortedArr = array.sort((a, b) => a - b);
+  return sortedArr[sortedArr.length - 1];
+  };
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(array === undefined) {
+    return 0;
+  } else if(array.length === 0) {
+    return 0;
+  };
+  let sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  let averageMean = sum / array.length;
+  return averageMean;
+  };
+
